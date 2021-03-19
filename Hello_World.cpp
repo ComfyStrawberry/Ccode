1c45729e58396f959a5de46a0a618782b2e7fa48
StrawberryIdiot <72462624+StrawberryIdiot@users.noreply.github.com>
2021-03-19 08:55:01 -0700

    Add files via upload

---
+++ Hello_World.cpp
@@ -0,0 +1,13 @@
+// save as helloworld.cpp
+// compile g++ helloworld.cpp -o hw.exe
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World \n";
+    cout << "CCCCCC CCCCC MMMMMMM \n";
+    cout << "CCC    CCC   M  M  M \n";
+    cout << "CCCCC  CCCCC M  M  M \n"; 
+
+    return 0;
+}
