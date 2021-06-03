1efe4dbd6c79ff47683fdf721aba447e3dfe278b
ComfyStrawberry <72462624+ComfyStrawberry@users.noreply.github.com>
2021-06-03 12:51:17 -0700

    Create randTest.cpp

---
+++ randTest.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib> 
+#include <ctime> 
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+    srand((unsigned)time(0)); 
+    float i;
+    i = rand()*1000000; 
+    cout << i << "\n"; 
+}#include <cstdlib> 
+#include <ctime> 
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+    srand((unsigned)time(0)); 
+    float i;
+    i = rand()*1000000; 
+    cout << i << "\n"; 
+}
