ec740d842ca425c92fd4a5305d097da7d6b089a2
ComfyStrawberry <72462624+ComfyStrawberry@users.noreply.github.com>
2021-06-03 12:51:57 -0700

    Update randTest.cpp

--- randTest.cpp
+++ randTest.cpp
@@ -10,16 +10,4 @@ int main()
     float i;
     i = rand()*1000000; 
     cout << i << "\n"; 
-}#include <cstdlib> 
-#include <ctime> 
-#include <iostream>
-
-using namespace std;
-
-int main() 
-{ 
-    srand((unsigned)time(0)); 
-    float i;
-    i = rand()*1000000; 
-    cout << i << "\n"; 
 }
